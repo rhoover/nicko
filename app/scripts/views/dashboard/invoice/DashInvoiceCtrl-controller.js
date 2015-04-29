663da5fe6ac86bd63462f2b8f5b092c2276f2361
robinHoover <robin.hoover@gmail.com>
2015-04-29 16:21:48 -0400

    Field View and Invoice Tweaking

--- app/scripts/views/dashboard/invoice/DashInvoiceCtrl-controller.js
+++ app/scripts/views/dashboard/invoice/DashInvoiceCtrl-controller.js
@@ -5,25 +5,34 @@
         .module('nickoApp.views')
         .controller('DashInvoiceCtrl', DashInvoiceCtrl);
 
-    function DashInvoiceCtrl(userCompanyMetaStore) {
+    function DashInvoiceCtrl(userCompanyMetaStore, clientsList) {
         /*jshint validthis: true */
         var spk = this;
 
+        getClientMeta();
         getClients();
 
         ////////////////
 
-        function getClients() {
+        function getClientMeta() {
             var companyData = userCompanyMetaStore.fetchCache();
-                // .then(function (companyData) {
-                    console.log(companyData);
                     spk.company = companyData.companyname;
                     spk.addressone = companyData.addressone;
                     spk.addresstwo = companyData.addresstwo;
                     spk.city = companyData.city;
                     spk.state = companyData.state.abbreviation;
                     spk.zip = companyData.zip;
-                // });
+        }
+
+        function getClients() {
+            clientsList.fetchClients()
+                .then(function (clientsListData) {
+                    var pickMeClient = {
+                        fullname: 'Pick A Client'
+                    };
+                    clientsListData.splice(0, 0, pickMeClient);
+                    spk.clients = clientsListData;
+                });
         }
     }
 })();
